c0ff9d96335a7815f2ea2ccf3663255aa6f75dc6
Michał Bąk <michalbak000@gmail.com>
2025-02-25 21:40:35 +0100

    added tailwindcss to the project

--- src/components/DefaultHeader.jsx
+++ src/components/DefaultHeader.jsx
@@ -1,8 +1,8 @@
 export function DefaultHeader() {
   return (
-    <>
-      <h1>Your Journey to Coding Conf 2025 Starts Here!</h1>
+    <header>
+      <h1 className="">Your Journey to Coding Conf 2025 Starts Here!</h1>
       <p>Secure your spot at next year's biggest coding conference.</p>
-    </>
+    </header>
   )
 }
