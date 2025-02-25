4a07e798b246ba3835809b2c8853bb58c603525a
Michał Bąk <michalbak000@gmail.com>
2025-02-25 14:10:52 +0100

    added Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { DefaultHeader } from "./components/DefaultHeader";
+import { Logo } from "./components/Logo";
 
 function App() {
   return (
     <div className="App">
+      <Logo />
       <DefaultHeader />
     </div>
   );
---
+++ src/components/Logo.jsx
@@ -0,0 +1,5 @@
+export function Logo() {
+  return (
+    <img src="/assets/images/logo-full.svg" alt="logo" />
+  )
+}
