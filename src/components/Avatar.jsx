4507761fd1eba22e470fc4c958abf1139517e342
Michał Bąk <michalbak000@gmail.com>
2025-02-25 18:44:04 +0100

    added avatarUploaded box

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -2,20 +2,35 @@ export function Avatar() {
 
   const labelStles = {
     border: '1px solid black',
+    padding: '10px'
   }
 
   const errorAvatarStyles = {
     display: 'none',
   }
 
+  const avatarUploadedStyles = {
+    display: 'none',
+  }
+
   return (
     <div>
       <h3>Upload Avatar</h3>
+
       <label for="inputFile"  style={labelStles}>
         <input style={{display: 'none'}} id="inputFile" type="file"/>
         <img src="/assets/images/icon-upload.svg"/>
         Drag and drop or click to upload
       </label>
+
+      <div className="avatarUploaded" style={avatarUploadedStyles}>
+        <img src="" alt="" />
+        <div>
+          <button>Remove image</button>
+          <button>Change image</button>
+        </div>
+      </div>
+
       <p>Upload your photo (JPG or PNG, max size: 500KB).</p>
       
       <div className="errorAvatar" style={errorAvatarStyles}>
