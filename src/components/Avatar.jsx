df6f0f12a9c56f1e1f08a35d3e42dec02f95a7d5
Michał Bąk <michalbak000@gmail.com>
2025-02-26 11:45:22 +0100

    added avatar preview

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,21 +1,41 @@
+import { useState } from "react";
+
 export function Avatar() {
+
+  const [preview, setPreview] = useState(null);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result);
+      }
+      reader.readAsDataURL(file);
+      
+      const avatarInput1 = document.getElementById('avatarInput1');
+      avatarInput1.style.display = 'none';
+
+      const avatarInput2 = document.getElementById('avatarInput2');
+      avatarInput2.style.display = 'flex';
+    }
+  }
 
   return (
     <div>
       <h3>Upload Avatar</h3>
-
      
-      <label htmlFor="inputFile" className="flex flex-col items-center border border-dashed border-black rounded-lg p-10">
-        <input className="hidden" id="inputFile" type="file"/>
-        <img src="/assets/images/icon-upload.svg"/>
+      <label id="avatarInput1" htmlFor="inputFile" className="flex flex-col items-center border border-dashed border-black rounded-lg ">
+        <input className="hidden" id="inputFile" type="file" onChange={handleImageChange}/>
+        <img src="/assets/images/icon-upload.svg" className="w-15 h-15"/>
         Drag and drop or click to upload
       </label>
       
-      <div className="hidden">
-        <img src="null" alt="avatar" />
+      <div id="avatarInput2" className="hidden flex-col items-center border border-dashed border-black rounded-lg">
+        <img src={preview} alt="avatar" className="w-15 h-15"/>
         <div>
-          <button>Remove image</button>
-          <button>Change image</button>
+          <button className="bg-gray-400 rounded-md">Remove image</button>
+          <button className="bg-gray-400 rounded-md">Change image</button>
         </div>
       </div>
 
