8b4622f9f300eaa8a2f7e30bd8a9a70b79b48097
Michał Bąk <michalbak000@gmail.com>
2025-02-26 12:02:12 +0100

    added remove and change image methods

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -4,7 +4,7 @@ export function Avatar() {
 
   const [preview, setPreview] = useState(null);
 
-  const handleImageChange = (event) => {
+  const handleUploadImage = (event) => {
     const file = event.target.files[0];
     if (file) {
       const reader = new FileReader();
@@ -21,12 +21,25 @@ export function Avatar() {
     }
   }
 
+  const handleRemoveImage = () => {
+    setPreview(null);
+    const avatarInput1 = document.getElementById('avatarInput1');
+    avatarInput1.style.display = 'flex';
+
+    const avatarInput2 = document.getElementById('avatarInput2');
+    avatarInput2.style.display = 'none';
+  }
+
+  const handleChangeImage = () => {
+    document.getElementById("inputFile").click();
+  }
+  
   return (
     <div>
       <h3>Upload Avatar</h3>
      
       <label id="avatarInput1" htmlFor="inputFile" className="flex flex-col items-center border border-dashed border-black rounded-lg ">
-        <input className="hidden" id="inputFile" type="file" onChange={handleImageChange}/>
+        <input className="hidden" id="inputFile" type="file" onChange={handleUploadImage}/>
         <img src="/assets/images/icon-upload.svg" className="w-15 h-15"/>
         Drag and drop or click to upload
       </label>
@@ -34,8 +47,8 @@ export function Avatar() {
       <div id="avatarInput2" className="hidden flex-col items-center border border-dashed border-black rounded-lg">
         <img src={preview} alt="avatar" className="w-15 h-15"/>
         <div>
-          <button className="bg-gray-400 rounded-md">Remove image</button>
-          <button className="bg-gray-400 rounded-md">Change image</button>
+          <button className="bg-gray-400 rounded-md" onClick={handleRemoveImage}>Remove image</button>
+          <button className="bg-gray-400 rounded-md" onClick={handleChangeImage}>Change image</button>
         </div>
       </div>
 
