2b4c1305e900b000dc3b8bab682d90e8f53a2ab0
Michał Bąk <michalbak000@gmail.com>
2025-02-26 12:18:46 +0100

    added handleNameChange method and const name to store input value dynamically on change

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
+
 export function Name() {
 
-  const errorNameStyles = {
-    display: 'none',
-  }
+  const [name, setName] = useState("");
   
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  }
+
   return (
     <div>
-    <h3>Full Name</h3>
-    <input type="text" />
+      <h3>Full Name</h3>
+      <input 
+        id="name" 
+        type="text" 
+        className="border border-solid border-black rounded-md" 
+        onChange={handleNameChange}
+        value={name}
+      />
       <div className="hidden">
         <img src="assets\images\icon-info.svg" alt="info" />
         <span>Please enter your full name.</span>
