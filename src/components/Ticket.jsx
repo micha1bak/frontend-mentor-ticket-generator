b5afde7865731ffbeca0a22cc5bc66fadee9993f
Michał Bąk <michalbak000@gmail.com>
2025-03-05 13:53:37 +0100

    ticket

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -3,24 +3,24 @@ export function Ticket({avatar, name, username, formatedDate}) {
   return (
     <div id="ticket" className="
       aspect-[600/280]
-      hidden w-full h-full bg-cover bg-center bg-no-repeat
+      hidden w-full bg-cover bg-center bg-no-repeat
       bg-[url(/public/assets/images/pattern-ticket.svg)]
-      items-center justify-between
+      items-center my-20
     ">
-      <div className="flex flex-col h-full justify-between">
+      <div className="flex flex-col h-full justify-between w-[80%]">
 
-        <div className="flex justify-between w-[260px] h-[75.5px]">
-          <div className="w-[40px] mt-3 ml-3">
-            <img className="w-8 h-8" src="assets\images\logo-mark.svg" alt="logo"/>
+        <div className="flex justify-between w-full h-[50%]">
+          <div className="w-[25%] flex justify-center items-center">
+            <img className="md:w-14 md:h-14 lg:w-20 lg:h-20" src="assets\images\logo-mark.svg" alt="logo"/>
           </div>
           
-          <div className="w-[210px] mt-2">
-            <h2 className="font-bold text-2xl">Coding Conf</h2>
-            <span className="opacity-60 text-sm">{formatedDate + " / Austin, TX"}</span>
+          <div className="w-[75%] mt-2 md:mt-3 lg:mt-5">
+            <h2 className="h-[50%] font-bold text-2xl md:text-3xl lg:text-5xl">Coding Conf</h2>
+            <span className="h-[50%] opacity-60 text-sm md:text-[15px] lg:text-xl">{formatedDate + " / Austin, TX"}</span>
           </div>
         </div>
 
-        <div className="flex w-[260px] h-[75.5px]">
+        <div className="flex w-full h-[50%]">
           <div className="flex items-center justify-center w-[74.5px]">
            <img className="w-13 h-13 rounded-lg" src={avatar} alt="avatar"/>
           </div>
@@ -42,7 +42,7 @@ export function Ticket({avatar, name, username, formatedDate}) {
 
       <div className="
         flex justify-center items-center 
-        w-[60px] h-[149px] opacity-50
+        w-[20%] h-[149px] opacity-50
       ">
         <span className="
           [writing-mode:vertical-rl]
