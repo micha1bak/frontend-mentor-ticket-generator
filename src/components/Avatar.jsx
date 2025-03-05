930365505d361b8a0078bba95faee8c927f7e60a
Michał Bąk <michalbak000@gmail.com>
2025-03-05 13:17:17 +0100

    avatar

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,14 +1,14 @@
 export function Avatar({handleUploadImage, handleChangeImage, handleRemoveImage, avatar}) {
   
   return (
-    <div className="w-xs">
+    <div className="w-full aspect-[2/1]">
       <h3 id="avatarTitle" className="text-xl opacity-80 my-2">Upload Avatar</h3>
      
       <label id="avatarInput1" htmlFor="inputFile" className="
         flex flex-col items-center justify-center 
         outline outline-dashed outline-amber-50/60
         hover:cursor-pointer
-        my-2 h-32 p-3 
+        my-2 h-[70%] p-3 
         rounded-xl w-full bg-blue-100/5 backdrop-blur-lg
       ">
           <input className="hidden" id="inputFile" type="file" onChange={handleUploadImage}/>
@@ -19,9 +19,9 @@ export function Avatar({handleUploadImage, handleChangeImage, handleRemoveImage,
       <div id="avatarInput2" className="
         hidden flex-col items-center justify-center 
         outline outline-dashed outline-amber-50/60 
-        rounded-xl my-2 h-32 bg-blue-100/5 backdrop-blur-lg
+        rounded-xl my-2 h-[70%] bg-blue-100/5 backdrop-blur-lg
       ">
-        <img src={avatar} alt="avatar" className="w-12 h-12 mb-3 rounded-xl"/>
+        <img src={avatar} alt="avatar" className="w-20 aspect-square mb-3 rounded-xl"/>
         <div>
           <button className="bg-blue-100/5 backdrop-blur-lg rounded-md px-1.5 mx-1 underline underline-offset-2 cursor-pointer" onClick={handleRemoveImage}>Remove image</button>
           <button className="bg-blue-100/5 backdrop-blur-lg rounded-md px-1.5 mx-1 cursor-pointer" onClick={handleChangeImage}>Change image</button>
